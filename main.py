9dd6f82e1713566c4f1c8802b90ded28e099a670
Aboelgamel20 <e.nashaat50@outlook.sa>
2025-02-03 21:34:35 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv('.env')
+from routes import base
 
 app = FastAPI()
-
-@app.get('/welcome')
-def welcome():
-    return "welcome to the first trial of the program."
+app.include_router(base.base_router)
